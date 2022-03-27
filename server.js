5a5afd409eab563ad496035a9e2013bb00575f91
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-03-27 13:21:57 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,83 +1,83 @@
-const express = require('express'); //include express
-const app = express(); //express.js app
-const bodyParser = require('body-parser'); //middle ware
+// const express = require('express'); //include express
+// const app = express(); //express.js app
+// const bodyParser = require('body-parser'); //middle ware
 
-app.use(bodyParser.urlencoded({ extended: false}));
+// app.use(bodyParser.urlencoded({ extended: false}));
 
-//route to homepage
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-//route to login page
-app.get('/login.html', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/intro.html', (req, res) => {
-    res.sendFile(__dirname + '/intro.html');
-});
-
-app.get('/level1.html', (req, res) => {
-    res.sendFile(__dirname + '/level1.html');
-});
-
-app.get('/back-retry2.png', (req, res) => {
-    res.sendFile(__dirname + '/back-retry2.png');
-});
-
-app.get('/knight-hurt2-retry.png', (req, res) => {
-    res.sendFile(__dirname + '/knight-hurt2-retry.png');
-});
-
-app.get('/knight-retry3.png', (req, res) => {
-    res.sendFile(__dirname + '/knight-retry3.png');
-});
-
-app.get('/knight2-retry2.png', (req, res) => {
-    res.sendFile(__dirname + '/knight2-retry2.png');
-});
-
-app.get('/zombie1-retry2.png', (req, res) => {
-    res.sendFile(__dirname + '/zombie1-retry2.png');
-});
-
-app.get('/zombie1-hurt-retry2.png', (req, res) => {
-    res.sendFile(__dirname + '/zombie1-hurt-retry2.png');
-});
-
-app.get('/script.js', (req, res) => {
-    res.sendFile(__dirname + '/script.js');
-});
-
-app.get('/script2.js', (req, res) => {
-    res.sendFile(__dirname + '/script2.js');
-});
-
-app.get('/knight-hurt2-retry2.png', (req, res) => {
-    res.sendFile(__dirname + '/knight-hurt2-retry2.png');
-});
-
-//route to level2 page
-app.get('/level2.html*', (req, res) => {
-    res.sendFile(__dirname + '/level2.html');
-});
-
-
-//post request login
-// app.post('/RowdyHacks', (req, res) => {
-//     let username = req.body.username;
-//     let password = req.body.password;
-//     res.send('Username: ${username} Password ${password}');
+// //route to homepage
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
 // });
-// //post request register
-// app.post('/RowdyHacks', (req, res) => {
-//     let firstName = req.body.firstName;
-//     let lastName = req.body.lastName;
-// })
-//asign port
-const port = 8080
-//fuction to listen to port
-app.listen(port, () => console.log(`is on port ${port}`));
+
+// //route to login page
+// app.get('/login.html', (req, res) => {
+//     res.sendFile(__dirname + '/login.html');
+// });
+
+// app.get('/intro.html', (req, res) => {
+//     res.sendFile(__dirname + '/intro.html');
+// });
+
+// app.get('/level1.html', (req, res) => {
+//     res.sendFile(__dirname + '/level1.html');
+// });
+
+// app.get('/back-retry2.png', (req, res) => {
+//     res.sendFile(__dirname + '/back-retry2.png');
+// });
+
+// app.get('/knight-hurt2-retry.png', (req, res) => {
+//     res.sendFile(__dirname + '/knight-hurt2-retry.png');
+// });
+
+// app.get('/knight-retry3.png', (req, res) => {
+//     res.sendFile(__dirname + '/knight-retry3.png');
+// });
+
+// app.get('/knight2-retry2.png', (req, res) => {
+//     res.sendFile(__dirname + '/knight2-retry2.png');
+// });
+
+// app.get('/zombie1-retry2.png', (req, res) => {
+//     res.sendFile(__dirname + '/zombie1-retry2.png');
+// });
+
+// app.get('/zombie1-hurt-retry2.png', (req, res) => {
+//     res.sendFile(__dirname + '/zombie1-hurt-retry2.png');
+// });
+
+// app.get('/script.js', (req, res) => {
+//     res.sendFile(__dirname + '/script.js');
+// });
+
+// app.get('/script2.js', (req, res) => {
+//     res.sendFile(__dirname + '/script2.js');
+// });
+
+// app.get('/knight-hurt2-retry2.png', (req, res) => {
+//     res.sendFile(__dirname + '/knight-hurt2-retry2.png');
+// });
+
+// //route to level2 page
+// app.get('/level2.html*', (req, res) => {
+//     res.sendFile(__dirname + '/level2.html');
+// });
+
+
+// //post request login
+// // app.post('/RowdyHacks', (req, res) => {
+// //     let username = req.body.username;
+// //     let password = req.body.password;
+// //     res.send('Username: ${username} Password ${password}');
+// // });
+// // //post request register
+// // app.post('/RowdyHacks', (req, res) => {
+// //     let firstName = req.body.firstName;
+// //     let lastName = req.body.lastName;
+// // })
+// //asign port
+// const port = 8080
+// //fuction to listen to port
+// app.listen(port, () => console.log(`is on port ${port}`));
 
 
