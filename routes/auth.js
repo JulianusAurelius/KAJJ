e179f8c72673f71219bb678b6ba6d693a4a8f8a2
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-03-27 13:04:36 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,28 @@ const express = require('express');
 const authController = require('../controllers/auth');
 const router = express.Router();
 const app = express();
+//const Knex = require('knex');
+const mysql = require('mysql');
 //create controller
 router.post('/register', authController.register);
+
+
+
+const connectWithUnixSockets = async config => {
+  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: 'root', // e.g. 'my-db-user'
+    password: 'u6pogl3ysxP7ehBp',
+    database: 'hackathon-2022-db',
+    // If connecting via unix domain socket, specify the path
+    socketPath: 'adroit-groove-345318:us-central1:hackathon-2022-db',
+   ...config
+  });
+};
+
+
+
 
 //get login info
 app.post('/login', (req, res) => {
@@ -22,6 +42,17 @@ app.post('/login', (req, res) => {
         let name_h = crypto.createHash('sha512').update(username_hash).digest('hex');
 
         //COMPARE PASSWORD COLUMN WITH password_h FROM DATABASE WHERE USER == name_h
+        con.connect(function(err) {
+            if (err) throw err;
+            let result = con.query("SELECT Login_name FROM Account WHERE Password='"+name+"'", function (err, result, fields) {
+              if (err) throw err;
+              console.log(result);
+            });
+            if (result == password_h)
+            {
+                res.send("Success");
+            }
+        });
     }
 });
 
@@ -42,8 +73,21 @@ app.post('/register', (req, res) => {
         let name_h = crypto.createHash('sha512').update(username_hash).digest('hex');
         
         //ADD ALL ITEMS TO APPROPRIATE
+        //Add to Account
+        con.connect(function(err) {
+            if (err) throw err;
+            let result = con.query("INSERT INTO Account (Login_name, Password) VALUES ('"+name+"','"+password_h+"')", function (err, result, fields) {
+              if (err) throw err;
+                console.log(result);
+            });
+        });
     }
 });
 
 
 module.exports = router; 
+
+const closeConnection = () => {
+  if (!knex) knex.destroy();
+  logger.info('DB connection closed.');
+};
