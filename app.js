f6e95d407441ef54d7845cc00e755c5b5a2fad3d
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-03-27 10:06:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -39,3 +39,54 @@ app.use('/auth', require('./routes/auth'));
 app.listen(8080, () => { 
     console.log('Server started on port 5001');
 })
+
+//For Courses
+app.get('/intro.html', (req, res) => {
+    res.sendFile(__dirname + '/intro.html');
+});
+
+app.get('/level1.html', (req, res) => {
+    res.sendFile(__dirname + '/level1.html');
+});
+
+app.get('/back-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/back-retry2.png');
+});
+
+app.get('/knight-hurt2-retry.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-hurt2-retry.png');
+});
+
+app.get('/knight-retry3.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-retry3.png');
+});
+
+app.get('/knight2-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/knight2-retry2.png');
+});
+
+app.get('/zombie1-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/zombie1-retry2.png');
+});
+
+app.get('/zombie1-hurt-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/zombie1-hurt-retry2.png');
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/script.js');
+});
+
+app.get('/script2.js', (req, res) => {
+    res.sendFile(__dirname + '/script2.js');
+});
+
+app.get('/knight-hurt2-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-hurt2-retry2.png');
+});
+
+//route to level2 page
+app.get('/level2.html*', (req, res) => {
+    res.sendFile(__dirname + '/level2.html');
+});
+
