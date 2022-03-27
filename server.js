9252c4b64fee265905dc2d31e9820ec673712e42
Julianus Aurelius <61988360+JulianusAurelius@users.noreply.github.com>
2022-03-27 03:34:55 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -14,20 +14,67 @@ app.get('/login.html', (req, res) => {
     res.sendFile(__dirname + '/login.html');
 });
 
-//post request login
-app.post('/RowdyHacks', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    res.send('Username: ${username} Password ${password}');
+//route to level2 page
+app.get('/level1.html', (req, res) => {
+    res.sendFile(__dirname + '/level1.html');
 });
-//post request register
-app.post('/RowdyHacks', (req, res) => {
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-})
+
+app.get('/back-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/back-retry2.png');
+});
+
+app.get('/knight-hurt2-retry.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-hurt2-retry.png');
+});
+
+app.get('/knight-retry3.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-retry3.png');
+});
+
+app.get('/knight2-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/knight2-retry2.png');
+});
+
+app.get('/zombie1-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/zombie1-retry2.png');
+});
+
+app.get('/zombie1-hurt-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/zombie1-hurt-retry2.png');
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(__dirname + '/script.js');
+});
+
+app.get('/script2.js', (req, res) => {
+    res.sendFile(__dirname + '/script2.js');
+});
+
+app.get('/knight-hurt2-retry2.png', (req, res) => {
+    res.sendFile(__dirname + '/knight-hurt2-retry2.png');
+});
+
+//route to level2 page
+app.get('/level2.html*', (req, res) => {
+    res.sendFile(__dirname + '/level2.html');
+});
+
+
+//post request login
+// app.post('/RowdyHacks', (req, res) => {
+//     let username = req.body.username;
+//     let password = req.body.password;
+//     res.send('Username: ${username} Password ${password}');
+// });
+// //post request register
+// app.post('/RowdyHacks', (req, res) => {
+//     let firstName = req.body.firstName;
+//     let lastName = req.body.lastName;
+// })
 //asign port
 const port = 8080
 //fuction to listen to port
-app.listen(port, () => console.log('is on port ${port}'));
+app.listen(port, () => console.log(`is on port ${port}`));
 
 
